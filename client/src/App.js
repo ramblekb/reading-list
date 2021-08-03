21f98cb0523210ff3bea6c437bdd487f5d483dcc
ramblekb <ramblekb@gmail.com>
2021-08-03 16:06:43 -0400

    intial

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import { TableContainer } from '@material-ui/core';
 import './App.css';
+import BTable from './components/BTable';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <TableContainer>
+
+     <BTable />
+     </TableContainer>
+    );
 }
 
 export default App;
