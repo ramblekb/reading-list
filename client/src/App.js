cc374af54bb76fc861af381007c7282b74ceb216
ramblekb <ramblekb@gmail.com>
2021-08-03 16:22:46 -0400

    shopping girls

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import { TableContainer } from '@material-ui/core';
-import './App.css';
-import BTable from './components/BTable';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <TableContainer>
-
-     <BTable />
-     </TableContainer>
-    );
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
